49897809d017dd34d4faf919f2722f32093cf879
BrunoTumelero <Bruno.Tumelero@outlook.com>
2022-01-23 22:17:03 -0300

    links coleted

--- zattini.py
+++ zattini.py
@@ -14,8 +14,18 @@ class_link = []
 '''for link_class in bs.find_all('figure', {'class': 'categorias-figure'}):
     print(link_class.text)'''
 
-for link_class in bs.find_all('figure', {'class': 'categorias-figure'}):
-    link = link_class.findChildren('a')
-    class_link.append(link[0].get('href'))
+for link_class in bs.find_all('li', {'class': 'drezzup-categorias-li'}):
+    link = link_class.findChild('a')
+    class_link.append(link.get('href'))
 
-print(class_link)
+    #print(link)
+#'https://www.zattini.com.br/calcados/masculino?mi=ztt_mntop_MASC-CAL-vermais&psn=Menu_Top&fc=menu'
+pesq1 = 'https://www.zattini.com.br/calcados/masculino?mi=ztt_hm_masc_cat1_calcadosmasc&psn=Banner_BarradeCategorias_1masc&fc=barradecategorias'
+html_pesq1 = Request(base, headers={'User-Agent': 'Mozilla/5.0'})
+url_pesq1 = urlopen(html_pesq1).read()
+bs_pesq1 = BeautifulSoup(url_pesq1, 'html5lib')
+print('Pesquisando em:', pesq1)
+
+for raw_data in bs_pesq1.find_all('div', {'class': 'item-list'}):
+    print(raw_data)
+
